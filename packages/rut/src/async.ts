8e3bd17e1fbaa9c7b688df79f153d5be5e571fde
Miles Johnson <mileswjohnson@gmail.com>
2019-08-22 20:16:34 -0700

    Add timer support.

--- packages/rut/src/async.ts
+++ packages/rut/src/async.ts
@@ -1,12 +1,86 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-const NativePromise = (global.Promise as any) as PromiseConstructor;
-const methodsToWrap: (keyof PromiseConstructor)[] = ['all', 'race', 'resolve', 'reject'];
+export type AsyncQueue = Set<Promise<any>>;
 
-export function wrapAndCaptureAsync(): Promise<any>[] {
-  const queue: Promise<any>[] = [];
+export type DelayedTimer = (
+  callback: (...args: any[]) => void,
+  delay: number,
+  ...args: any[]
+) => NodeJS.Timeout;
 
-  function MockPromise<T>(
+export type ScheduledTimer = (callback: (value: any) => void, ...args: any[]) => number;
+
+declare global {
+  interface Window {
+    requestIdleCallback: ScheduledTimer;
+  }
+}
+
+function findAndPreserveNative<T>(name: string, bind: boolean = true): T {
+  let func = () => {};
+
+  if (typeof global !== 'undefined' && name in global) {
+    func = (global as any)[name];
+
+    if (bind) {
+      func = func.bind(global);
+    }
+  } else if (typeof window !== 'undefined' && name in window) {
+    func = (window as any)[name];
+
+    if (bind) {
+      func = func.bind(window);
+    }
+  }
+
+  // @ts-ignore
+  return func;
+}
+
+const nativeRequestAnimationFrame = findAndPreserveNative<ScheduledTimer>('requestAnimationFrame');
+const nativeRequestIdleCallback = findAndPreserveNative<ScheduledTimer>('requestIdleCallback');
+const nativeSetInterval = findAndPreserveNative<DelayedTimer>('setInterval');
+const nativeSetTimeout = findAndPreserveNative<DelayedTimer>('setTimeout');
+const NativePromise = findAndPreserveNative<PromiseConstructor>('Promise', false);
+
+export function createFakeDelayedTimer(timer: DelayedTimer, queue: AsyncQueue): DelayedTimer {
+  return (callback, ms, ...args) => {
+    let resolver: () => void;
+
+    queue.add(
+      new Promise(resolve => {
+        resolver = resolve;
+      }),
+    );
+
+    return timer(() => {
+      resolver();
+      callback(...args);
+    }, ms);
+  };
+}
+
+export function createFakeScheduledTimer(timer: ScheduledTimer, queue: AsyncQueue): ScheduledTimer {
+  return (callback, ...args) => {
+    let resolver: () => void;
+
+    queue.add(
+      new Promise(resolve => {
+        resolver = resolve;
+      }),
+    );
+
+    return timer(value => {
+      resolver();
+      callback(value);
+    }, ...args);
+  };
+}
+
+const promiseMethodsToWrap: (keyof PromiseConstructor)[] = ['all', 'race', 'resolve', 'reject'];
+
+export function wrapPromise(queue: AsyncQueue) {
+  function FakePromise<T>(
     executor: (
       resolve: (value?: T | PromiseLike<T>) => void,
       reject: (reason?: any) => void,
@@ -14,25 +88,42 @@ export function wrapAndCaptureAsync(): Promise<any>[] {
   ): Promise<T> {
     const promise = new NativePromise(executor);
 
-    queue.push(promise);
+    queue.add(promise);
 
     return promise;
   }
 
-  methodsToWrap.forEach(method => {
-    Object.defineProperty(MockPromise, method, {
+  promiseMethodsToWrap.forEach(method => {
+    Object.defineProperty(FakePromise, method, {
       value: (...args: unknown[]) => {
         // @ts-ignore
         const promise = NativePromise[method](...args);
 
-        queue.push(promise);
+        queue.add(promise);
 
         return promise;
       },
     });
   });
 
-  global.Promise = MockPromise;
+  global.Promise = FakePromise;
+}
+
+export function wrapTimers(queue: AsyncQueue) {
+  if (typeof window !== 'undefined') {
+    window.requestAnimationFrame = createFakeScheduledTimer(nativeRequestAnimationFrame, queue);
+    window.requestIdleCallback = createFakeScheduledTimer(nativeRequestIdleCallback, queue);
+  }
+
+  global.setInterval = createFakeDelayedTimer(nativeSetInterval, queue);
+  global.setTimeout = createFakeDelayedTimer(nativeSetTimeout, queue);
+}
+
+export function wrapAndCaptureAsync(): AsyncQueue {
+  const queue: AsyncQueue = new Set();
+
+  wrapPromise(queue);
+  wrapTimers(queue);
 
   return queue;
 }
@@ -41,8 +132,19 @@ export function unwrapPromise() {
   global.Promise = NativePromise;
 }
 
-export function waitForAsyncQueue(queue: Promise<any>[]) {
-  unwrapPromise();
+export function unwrapTimers() {
+  if (typeof window !== 'undefined') {
+    window.requestAnimationFrame = nativeRequestAnimationFrame;
+    window.requestIdleCallback = nativeRequestIdleCallback;
+  }
 
-  return NativePromise.all(queue);
+  global.setInterval = nativeSetInterval;
+  global.setTimeout = nativeSetTimeout;
+}
+
+export function waitForAsyncQueue(queue: AsyncQueue) {
+  unwrapPromise();
+  unwrapTimers();
+
+  return NativePromise.all(Array.from(queue));
 }
