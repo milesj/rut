d25d31d47e88c43912b0656e59b5712e6673bd2f
Miles Johnson <mileswjohnson@gmail.com>
2019-09-09 13:54:03 -0700

    Export Element and Result from index.

--- packages/rut/src/index.ts
+++ packages/rut/src/index.ts
@@ -6,6 +6,8 @@
 import { configure } from './configure';
 import matchers from './matchers';
 import { render, renderAndWait } from './render';
+import BaseElement from './Element';
+import BaseResult from './Result';
 
 export * from './helpers';
 export * from './mocks';
@@ -13,3 +15,7 @@ export * from './predicates';
 export * from './types';
 
 export { configure, matchers, render, renderAndWait };
+
+// We don't want to export the classes, only the types
+export type Element = typeof BaseElement;
+export type Result = typeof BaseResult;
